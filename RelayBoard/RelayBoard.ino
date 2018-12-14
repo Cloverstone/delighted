ed4566758fdd399d2e1e1efe3d4a8516dfcc96d4
Adam Smallcomb <adam@users-air.lan>
2018-12-14 08:18:22 -0500

    Add Arduino code

---
+++ RelayBoard/RelayBoard.ino
@@ -0,0 +1,72 @@
+
+const int pins[] = {2,3,4,5,6,7,8,9};    // the number of the pushbutton pin
+
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+void setup() {
+
+  Serial.begin(9600);
+ 
+  for (int i=0; i<sizeof pins/sizeof pins[0]; i++) {
+    pinMode(pins[i], OUTPUT);
+        digitalWrite(pins[i],HIGH);
+
+  }
+}
+
+
+
+void execute(String inputString) {
+ 
+  if(inputString.startsWith("-")){    
+    int relay = inputString.substring(1,2).toInt();
+    digitalWrite(relay,HIGH);
+
+  }
+
+  if(inputString.startsWith("+")){    
+    int relay = inputString.substring(1,2).toInt();
+    digitalWrite(relay,LOW);
+
+  }
+  
+  if(inputString.startsWith("A")){    
+    int relay = inputString.substring(1,2).toInt();
+    digitalWrite(relay,LOW);
+
+  }
+
+
+}
+
+
+void loop() {
+
+  if (stringComplete) {    
+    Serial.println("Recieved:"+inputString);
+
+    execute(inputString);
+     
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+
+  
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '=') {
+      stringComplete = true;
+    }
+  }
+}
+
--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
-include 'PhpSerial.php';
+// include 'PhpSerial.php';
 
-// Let's start the class
-$serial = new PhpSerial;
+// // Let's start the class
+// $serial = new PhpSerial;
 
-// First we must specify the device. This works on both linux and windows (if
-// your linux serial device is /dev/ttyS0 for COM1, etc)
-$serial->deviceSet("COM1");
+// // First we must specify the device. This works on both linux and windows (if
+// // your linux serial device is /dev/ttyS0 for COM1, etc)
+// $serial->deviceSet("/dev/cu.usbmodem1411");
 
-// We can change the baud rate, parity, length, stop bits, flow control
-$serial->confBaudRate(2400);
-$serial->confParity("none");
-$serial->confCharacterLength(8);
-$serial->confStopBits(1);
-$serial->confFlowControl("none");
+// // We can change the baud rate, parity, length, stop bits, flow control
+// $serial->confBaudRate(9600);
+// // $serial->confParity("none");
+// // $serial->confCharacterLength(8);
+// // $serial->confStopBits(1);
+// // $serial->confFlowControl("none");
 
-// Then we need to open it
-$serial->deviceOpen();
+// // Then we need to open it
+// $serial->deviceOpen();
 
-// To write into
-$serial->sendMessage("Hello !");
+// // To write into
+// $serial->sendMessage("+6=");
+// // Then we need to open it
+// $serial->deviceClose();
+
+
+$f = fopen("/dev/cu.usbmodem1421", "w");
+sleep(2);
+fwrite($f, '+6=');
+fclose($f);
+
+// $element = exec("cu -l /dev/cu.usbmodem1421;", $output);
+
+// 
+// exec("stty -f /dev/tty.usbmodem621 cs8 9600 -hup ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts");
